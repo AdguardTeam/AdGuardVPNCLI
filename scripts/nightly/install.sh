9204ca3237f4a7505b0193bea422081bed064e0f
Sergey Fionov <sfionov@adguard.com>
2026-07-10 15:44:04 +0200

    Fix download URL in install script
    Update download URL to remove the channel parameter.

--- scripts/nightly/install.sh
+++ scripts/nightly/install.sh
@@ -509,7 +509,7 @@ configure() {
     pkg_name="${exe_name}-${os}-${cpu}.${pkg_ext}"
   fi
   apply_version
-  url="https://github.com/AdguardTeam/AdGuardVPNCLI/releases/download/v${version}-${channel}/${pkg_name}"
+  url="https://github.com/AdguardTeam/AdGuardVPNCLI/releases/download/v${version}/${pkg_name}"
 
   readonly output_dir url pkg_name
 
